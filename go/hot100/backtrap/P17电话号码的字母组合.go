e59d73f2e0e6a0a7862ff9489684ac40f2de7b15
JiGuoDing <485204300@qq.com>
2026-03-11 20:51:14 +0800

    feat: 添加动态规划和回溯算法题解
    实现三个算法题的解决方案：
    1. P198打家劫舍 - 动态规划解决房屋抢劫问题
    2. P279完全平方数 - 动态规划解决完全背包问题
    3. P17电话号码的字母组合 - 回溯算法生成所有可能的字母组合
    
    Signed-off-by: JiGuoDing <485204300@qq.com>

---
+++ go/hot100/backtrap/P17电话号码的字母组合.go
@@ -0,0 +1,44 @@
+package backtrap
+
+func letterCombinations(digits string) []string {
+	// 处理边界情况
+	if len(digits) == 0 {
+		return []string{}
+	}
+
+	// Go 支持在切片/数组字面量中指定下标，未指定的位置自动补零值
+	digit2Letters := [][]byte{
+		2: []byte("abc"),
+		3: []byte("def"),
+		4: []byte("ghi"),
+		5: []byte("jkl"),
+		6: []byte("mno"),
+		7: []byte("pqrs"),
+		8: []byte("tuv"),
+		9: []byte("wxyz"),
+	}
+
+	var res []string
+	var combination []byte
+
+	var backtrap func(i int)
+	backtrap = func(i int) {
+		if len(combination) == len(digits) {
+			combinationStr := string(combination)
+			res = append(res, combinationStr)
+			// 注意这里万万不能清空 combination，下面的 combination = combination[:len(combination)-1] 会撤销选择，也即回溯
+			// backtracking 的"撤销"已经由 for 循环里的 combination[:len(combination)-1] 负责了
+			return
+		}
+
+		for _, letter := range digit2Letters[digits[i]-'0'] {
+			combination = append(combination, letter)
+			backtrap(i + 1)
+			combination = combination[:len(combination)-1]
+		}
+	}
+
+	backtrap(0)
+
+	return res
+}
---
+++ go/hot100/dynamicaProgramming/P198打家劫舍.go
@@ -0,0 +1,21 @@
+package dynamicaprogramming
+
+func rob(nums []int) int {
+	dp := make([]int, len(nums))
+	// 处理边界情况
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	// 要正确初始化
+	dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+	for i := 2; i < len(nums); i++ {
+		// 状态转移方程
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+	return dp[len(dp)-1]
+}
