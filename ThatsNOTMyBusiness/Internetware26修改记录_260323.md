fc8cab5afe5b669ee6395c78941514f935858c0e
JiGuoDing <485204300@qq.com>
2026-03-25 00:42:10 +0800

    feat: 添加周报和算法实现，更新论文修改记录
    - 添加第24周周报文件及PDF版本
    - 删除旧的第24周周报文件
    - 添加Internetware'26论文修改记录
    - 实现hot100中的P200岛屿数量算法
    
    Signed-off-by: JiGuoDing <485204300@qq.com>

---
+++ ThatsNOTMyBusiness/Internetware26修改记录_260323.md
@@ -0,0 +1,13 @@
+# Internetware'26 修改记录(2026.03.23)
+
+## 内容修改
+
+- 删去 CCS Concepts 和 ACM Reference Format 部分
+- 确认是 Research Track，Topic 为 "Operating System and Internetware" 或 "Software Models and Techniques for Internet-based Systems such as Cloud Computing, Service Computing, Social Computing, Mobile Internet, Internet of Things, and Cyber-Physical Systems" (暂定为后者)
+- 修改 Abstract, Introduction, Evaluation, Conclusion 以贴近 Topic
+
+## 图表修改
+
+- Figure 2 图片字体放大
+
+## 格式修改
--- go/hot100/graph/P200岛屿数量.go
+++ go/hot100/graph/P200岛屿数量.go
@@ -1,1 +1,52 @@
 package graph
+
+type coordinate struct {
+	x int
+	y int
+}
+
+func numIslands(grid [][]byte) int {
+	// 处理边界情况
+	if len(grid) == 0 {
+		return 0
+	}
+
+	iterationCnt := 0
+	rows, columns := len(grid), len(grid[0])
+	auxiliaryQueue := make([]coordinate, 0)
+	offsets := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+	for rowNum, row := range grid {
+		for columnNum, ele := range row {
+			// 如果该坐标是 0 说明是水域或者是已经被访问过的岛屿
+			if ele == '0' {
+				continue
+			}
+			// 该坐标是 1，说明是一个新的岛屿，BFS 遍历该岛屿的坐标
+			iterationCnt++
+			auxiliaryQueue = append(auxiliaryQueue, coordinate{rowNum, columnNum})
+			grid[rowNum][columnNum] = '0'
+			for len(auxiliaryQueue) != 0 {
+				// 弹出队头坐标
+				currentCoordinate := auxiliaryQueue[0]
+				auxiliaryQueue = auxiliaryQueue[1:]
+
+				for _, offset := range offsets {
+					nextCoordinateX, nextCoordinateY := currentCoordinate.x+offset[0], currentCoordinate.y+offset[1]
+					// 判断下一坐标是否合法
+					if nextCoordinateX >= 0 && nextCoordinateX < rows && nextCoordinateY >= 0 && nextCoordinateY < columns {
+						// 下一个坐标是水域，跳过
+						if grid[nextCoordinateX][nextCoordinateY] == '0' {
+							continue
+						}
+						// 下一个坐标是陆地，加入辅助队列
+						auxiliaryQueue = append(auxiliaryQueue, coordinate{nextCoordinateX, nextCoordinateY})
+						grid[nextCoordinateX][nextCoordinateY] = '0'
+					}
+				}
+			}
+		}
+	}
+
+	return iterationCnt
+}
